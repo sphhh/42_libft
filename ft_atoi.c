1255f7f729f4861928b2cfaec8b929ea910c3ba5
vipereir <zzvtrx@gmail.com>
2022-05-10 14:49:20 -0300

    envio to mal

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 11:40:23 by vipereir          #+#    #+#             */
-/*   Updated: 2022/05/09 12:13:26 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/05/10 14:35:29 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	ft_atoi(const char *str)
 	i = 0;
 	nb = 0;
 	signal = 1;
+	while (str[i] == '	')
+			i++;
 	if (str[i++] == '-')
 		signal = -signal;
 	while (str[i] >= '0' && str[i] <= '9' && str[i])
@@ -30,14 +32,14 @@ int	ft_atoi(const char *str)
 	}
 	return (nb * signal);
 }
-/*
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int	main(void)
 {
-	char	*num = "0";
+	char	*num = "       353";
 	printf("%d\n", atoi(num));
 	printf("%d\n", ft_atoi(num));
 	return (0);
-}*/
+}
