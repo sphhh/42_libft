943de09c0901b5f86ac876fad906d5e62911ba02
vipereir <zzvtrx@gmail.com>
2022-05-13 09:27:54 -0300

    strlcar

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 15:00:25 by vipereir          #+#    #+#             */
-/*   Updated: 2022/05/12 15:49:36 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/05/13 09:26:38 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	a;
-	size_t	max_len;
 
 	i = 0;
 	a = 0;
-	max_len = ft_strlen(dst) - 1;
-	while (dst[a])
+	while (dst[a] && a < size)
 		a++;
-	while (src[i] && i < size && i < max_len)
-		dst[a++] = src[i++];
-	dst[a] = '\0';
-	return (max_len + ft_strlen((char *)src));
+	while ((src[i]) && (i + a + 1) < size)
+	{
+		dst[a + i] = src[i];
+		i++;
+	}
+	if (a != size)
+		dst[a + i] = '\0';
+	return (a + ft_strlen((char *)src));
 }
 
 #include <stdio.h>
@@ -44,15 +46,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 int	main(void)
 {
-	char src[42] = "aaaaaaaaiaaaaaaaaaaaaaa";
-	char sr2[42] = "aaaaaaaaiaaaaaaaaaaaaaaa";
+	char src[42] = "aaaaaaaaaaaaaaaaaaa";
+	char sr2[42] = "aaaaaaaaaaaaaaaaaaa";
 	char dest[42] = "bbbbb";
 	char des2[42] = "bbbbb";
 // tentar implementar igual o da apple.
-	printf("%lu\n", strlcat(dest, src, 16));
+	printf("%lu\n", strlcat(dest, src, 20));
 	printf("%s\n", dest);
 
-	printf("%lu\n", ft_strlcat(des2, sr2, 16));
+	printf("%lu\n", ft_strlcat(des2, sr2, 20));
 	printf("%s\n", des2);
+	return(0);
 }
 
