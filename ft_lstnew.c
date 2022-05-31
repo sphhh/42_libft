d6de160cf216708db462990d6cc989c1d28e8ba6
vipereir <zzvtrx@gmail.com>
2022-05-31 15:49:40 -0300

    lstnew start

---
+++ ft_lstnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 11:09:50 by vipereir          #+#    #+#             */
+/*   Updated: 2022/05/31 15:36:42 by vipereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstnew(void *content)
+{
+	t_list *new;
+
+	new->content = malloc(sizeof(void *));
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	void	*content = "123456";
+
+	ft_lstnew(content);
+	printf("%s\n", );
+	return (0);
+}
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,16 +6,28 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 13:54:25 by vipereir          #+#    #+#             */
-/*   Updated: 2022/05/30 10:52:02 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/05/31 10:52:35 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 
 void	*ft_memmove(void	*dst, const void	*src, size_t	n)
 {
-	ft_memcpy(dst, src, n);
+	size_t	i;
+
+	i = n - 1;
+	if (dst < (void *)src)
+		ft_memcpy(dst, src, n);
+	if (dst > (void *)src)
+	{
+		while (i >= 0)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i--;
+		}
+		return (dst);
+	}
 	return (dst);
 }
 
@@ -26,15 +38,15 @@ void	*ft_memmove(void	*dst, const void	*src, size_t	n)
 int	main(void)
 {
 	// memmove use a buffer and memcpy does not;	
-	char	str[] = "aaaaa bbbb ccc dd e";
+	char	str[42] = "123456789";
 	//char	dest[] = "";
 
-	char	str2[] = "aaaaa bbbb ccc dd e";
+	char	str2[42] = "123456789";
 	//char	dest2[] = "";
 
-	memmove(str, str + 5, 10);
+	memmove(str + 3, str, 10);
 	printf("%s\n", str);
-	ft_memmove(str2, str2 + 5, 10);
+	ft_memmove(str2 + 3, str2, 10);
 	printf("%s\n", str2);
 	
 	return (0);
